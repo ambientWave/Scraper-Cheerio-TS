ff85c99b16700229d237d65b292907f2747c3c8f
ambientWave <minaw368@gmail.com>
2026-08-28 13:35:45 +0300

    Stage 1: fetch and cache HTML

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-import { RequestService } from './services/request.service.ts';
+import { RequestService } from './services/request.service';
+import { CacheService } from './services/cache.service';
+import { CheerioAPI } from 'cheerio';
 
-async function main() {
-    const robotsRes = await RequestService.request("https://books.toscrape.com/robots.txt");
-    if (robotsRes.status !== 200) {
-        console.log("no robots file found");
-    }
+async function main(url: string) {
+    // Initialize the cache
+    await CacheService.initialize();
+
+    // Check if a URL is cached
+    const hasCached: boolean = await CacheService.has(url);
+    if (hasCached) {
+        console.log("CACHE HIT: Using cached response");
+        const cachedContent: CheerioAPI | null = await CacheService.get(url);
+        console.log(`Page Size: ${Buffer.from(cachedContent?.text() || "").length}`);
+        return;
+    };
+    console.log("FETCH: Fetching from origin");
+    const pageRes: Response = await RequestService.request(url);
+    const htmlContent: string = await pageRes.text();
+    console.log(`Page Size: ${Buffer.from(htmlContent).length}`);
+    await CacheService.set(url, htmlContent);
 };
 
-main();
-
-
+main("https://books.toscrape.com/catalogue/page-1.html");
--- src/services/request.service.ts
+++ src/services/request.service.ts
@@ -4,7 +4,26 @@ export class RequestService {
     };
 
     public static async request(url: string, options?: RequestInit) {
-        const response = await this.fetchResource(url, options)
+        if (!url) {
+            throw new Error("URL is required");
+        }
+        const reqOptions: RequestInit = { ...options };
+        reqOptions.headers = {
+            ...options?.headers,
+            "User-Agent": "FlyRankInternship-A9/1.0 (https://github.com/ambientWave/Scraper-Cheerio-TS)"
+        };
+        reqOptions.signal = AbortSignal.timeout(3000);
+        const urlRoot: string = new URL(url).origin;
+        const robotsRes: Response = await this.fetchResource((urlRoot + "/robots.txt"), reqOptions);
+        console.log(`Robots Response Code: ${robotsRes.status}`);
+        if (robotsRes.status !== 200) {
+            console.log("no robots file found");
+        }
+        const response: Response = await this.fetchResource(url, reqOptions);
+        console.log(`Page Response Code: ${response.status}`);
+        if (response.status !== 200) {
+            throw new Error("Request failed");
+        }
         return response
     }
-}
+};
